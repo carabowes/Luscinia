d7b69597af12896dd337c1a55b4bee8877640881
“Jiayu <2721346L@student.gla.ac.uk>
2024-11-22 03:39:55 +0000

    Added basic map zooming functionality

--- luscinia/scenes/map.gd
+++ luscinia/scenes/map.gd
@@ -1,7 +1,9 @@
 extends Control
 
 var current_scale : float = 1
-@export var zoom_speed : float = 0.5
+@export var zoom_speed : float = 0.1
+@export var min_zoom : float = 0.5  
+@export var max_zoom : float = 2.0
 @export var pan_speed : float = 500
 @onready var map = $MapTexture
 
@@ -20,5 +22,18 @@ func _process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_D):
 		map.position.x -= pan_speed * delta
 		
-		
+	current_scale = clamp(current_scale, min_zoom, max_zoom)
 	map.scale = Vector2.ONE * current_scale
+	
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			current_scale += zoom_speed
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			current_scale -= zoom_speed
+			
+		current_scale = clamp(current_scale, min_zoom, max_zoom)
+		map.scale = Vector2.ONE * current_scale
+		
+		
+		
