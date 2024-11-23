023efbf30e87fc78d80a7334a1c58bbfcb079339
ryan-bell <2752881B@student.gla.ac.uk>
2024-11-23 14:07:06 +0000

    Themes and Scope Change
    Refs #60
    
    Added themes to the UI so its easier to repeat.
    Some extra clean up with some unused script that are no longer needed
    for the UI.
    Changing scope, so that staff on task and task log are currently not
    needed in the task page. This should greatly increase the chance of
    getting a working prototype by the next sprint.

--- luscinia/scripts/StaffEntry.gd
+++
@@ -1,9 +0,0 @@
-class_name StaffEntry
-extends Resource
-
-@export var name : String
-@export var image : Texture2D
-
-func _init(name : String, image : Texture2D):
-	self.name = name
-	self.image = image
