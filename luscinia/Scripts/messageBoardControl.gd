9d24d2ce686e024547194184b6047183c7ba4c20
nootlynoot <huxiaozu1245@gmail.com>
2024-11-27 07:23:07 +0800

    Dynamically add message contents and choice buttons. Ref #57
    - Created choice button script
    
    - Added functionality in message box to hold how many messages a text message would have
    - Added functionality in message box to hold what the contents of the message will be
    - Added functionality to change message text dynamically in message box
    - Added functionality to delete text messages dynamically upon make a choice
    - Added functionality to return to main screen upon making a choice
    
    - Fixed bug mentioned in 9f34027d where button is out of frame in play mode by changing the min sizing of parent object
    
    Notes:
    Might want to consider changing from dynamic addition to hard coding with help of prefabs from sprint 3 onwards

--- luscinia/Scripts/messageBoardControl.gd
+++ luscinia/Scripts/messageBoardControl.gd
@@ -1,9 +1,31 @@
 extends VBoxContainer
 
 @onready var gm = $"../../.."
+@onready var message_list = $"../.."
+#amount of incoming messages based on current iteration of gm msg loop
+@export var msg_amt: Array[int] = []
+#content of incoming messages based on current iteration of gm msg loop
+@export var msg_content: Array[String] = []
+
+@export var to_be_disabled: Array[Button] = []
+@export var msg_shown: int
 
 func add_message():
 	var message_instance = load("res://Nodes/button.tscn").instantiate()
 	message_instance.text = gm.messages[gm.message_amount-1]
 	add_child(message_instance)
 	move_child(message_instance, 0)
+
+func add_button(b : Button):
+	to_be_disabled.append(b)
+
+func disable_buttons():
+	for i in to_be_disabled:
+		i.visible = false
+		
+func enable_buttons():
+	for i in to_be_disabled:
+		i.visible = true
+
+func reset():
+	message_list.visible = false
--- luscinia/Scripts/messageButton.gd
+++ luscinia/Scripts/messageButton.gd
@@ -1,20 +1,31 @@
 extends Node
 
-@onready var gm = $"../../../.."
-
 var messageScene = preload("res://Nodes/text_message.tscn")
-
+@export var to_be_deleted: Array[ColorRect] = []
+@onready var msg_board = $".."
+@onready var gm = $"../../../.."
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.pressed.connect(self.button_pressed)
 
-
 func button_pressed():
-	var message = messageScene.instantiate()
-	var message2 = messageScene.instantiate()
-	add_child(message)
-	add_child(message2)
-	message2.position += Vector2(0,108)
-	#message.layout.transform.position.x += 16
+	msg_board.disable_buttons()
+	for i in msg_board.msg_amt[gm.message_amount-1]:
+		var message = messageScene.instantiate()
+		if i != 1:
+			message.position += Vector2(0,108)
+		message.get_child(0).text = msg_board.msg_content[msg_board.msg_shown]
+		msg_board.msg_shown += 1
+		to_be_deleted.append(message)
+		add_child(message)
+	msg_board.add_button(self)
 	get_child(0).visible = true
-	#get_parent().get_parent().get_parent().visible = false
+	
+func choice_picked():
+	for i in to_be_deleted:
+		i.free()
+	to_be_deleted = []
+	msg_board.reset()
+	msg_board.enable_buttons()
+	get_child(0).visible = false
+	
