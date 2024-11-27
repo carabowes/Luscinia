c8bcf523df360ab5d10124decafef75b605e5351
jackporter04 <2756698P@student.gla.ac.uk>
2024-11-27 12:37:40 +0000

    basic resource manager

---
+++ luscinia/Scripts/resource_manager.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+var resources = {"people": 100, "funds": 1000, "vehicles": 80, "supplies": 100000}
+
+func add_resources(resource_name: String, amount: int):
+	if resource_name in resources:
+		resources[resource_name] += amount
+	else:
+		print("Resource not found:", resource_name)
+
+func remove_resources(resource_name: String, amount: int):
+	if resource_name in resources:
+		resources[resource_name] -= amount
+		if resources[resource_name] < 0:
+			resources[resource_name] = 0
+	else:
+		print("Resource not found:", resource_name)
+
+
+
+
+		
---
+++ luscinia/Scripts/resource_page.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _on_return_button_pressed() -> void:
+	print(ResourceManager.resources["funds"])
