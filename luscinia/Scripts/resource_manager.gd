22eb8ce0c001a5e07363aea52775773cca81d05d
jackporter04 <2756698P@student.gla.ac.uk>
2024-11-27 16:03:36 +0000

    Basic Resources Page

--- luscinia/Scripts/resource_manager.gd
+++ luscinia/Scripts/resource_manager.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 var resources = {"people": 100, "funds": 1000, "vehicles": 80, "supplies": 100000}
+var available_resources = {"people": 100, "vehicles": 80, "supplies": 100000}
 
 func add_resources(resource_name: String, amount: int):
 	if resource_name in resources:
--- luscinia/Scripts/resource_page.gd
+++ luscinia/Scripts/resource_page.gd
@@ -1,5 +1,33 @@
-extends Node2D
+extends Control
+
+var resources = {}
+var available_resources = {}
+
+func _ready() -> void:
+	resources = ResourceManager.resources
+	available_resources = ResourceManager.available_resources
+	update_all_labels()
+	
+	
 
 
 func _on_return_button_pressed() -> void:
 	print(ResourceManager.resources["funds"])
+	
+func update_label(label_name: String, resource_name: String):
+	var label = get_node(label_name)
+	if label and resource_name in resources:
+		if resource_name != "funds":
+			label.text = str(available_resources.get(resource_name, 0)) + " / " + str(resources[resource_name])
+		else:
+			label.text = str(resources[resource_name]) + " Million"
+	else:
+		print("Label or resource not found:", label_name, resource_name)
+
+
+func update_all_labels() -> void:
+	update_label("Personel Output", "people")
+	update_label("Funding Output", "funds")
+	update_label("Vehicles Output", "vehicles")
+	update_label("Supplies Output", "supplies")
+	
