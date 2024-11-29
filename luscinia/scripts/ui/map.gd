2fbdda0b403532810db05de86d06da548e5ca8c0
ryan-bell <2752881B@student.gla.ac.uk>
2024-11-29 14:01:22 +0000

    Minor Bug Fixes and Style Changes
    Refs #48
    
    Fixed some bugs with the clamping as it would not always
    fully clamp the map inside the viewport. Also made it so that
    the viewport size and map size are auto generated rather than being
    export variables.
    
    Changed the code layout to match the agreed style guide.

--- luscinia/scripts/ui/map.gd
+++ luscinia/scripts/ui/map.gd
@@ -1,23 +1,24 @@
 extends Control
 
-var current_scale : float = 1
 @export var zoom_speed : float = 0.1
 @export var min_zoom : float = 0.5  
 @export var max_zoom : float = 2.0
 @export var pan_speed : float = 500
 @onready var map = $MapTexture
 
+var current_scale : float = 1
 var is_dragging = false
 var last_mouse_position = Vector2.ZERO
 
-@export var map_size = Vector2(3500,1866)
-@export var viewport_size = Vector2(1054, 933)
+@onready var map_size = map.size
+@onready var viewport_size = Vector2(get_viewport().get_visible_rect().size)
 
 func _ready() -> void:
+	print(viewport_size)
+	print(map_size)
 	var initial_position = (viewport_size - map_size * current_scale) / 2
 	map.position = initial_position
 	clamp_position()
-	
 
 
 func _process(delta: float) -> void:
@@ -33,54 +34,39 @@ func _process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_D):
 		map.position.x -= pan_speed * delta
 		clamp_position()
-		
-	
+
+
 func _input(event):
-		
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
 			handle_wheel_input(zoom_speed, get_global_mouse_position())
 			
 		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			handle_wheel_input(-zoom_speed, get_global_mouse_position())
-			
-			
-			
+
+
 func handle_wheel_input(delta_zoom: float, global_mouse_position: Vector2):
 	var local_mouse_position = global_mouse_position - map.position
 	var prev_scale = current_scale
 	current_scale += delta_zoom
 	current_scale = clamp(current_scale, min_zoom, max_zoom)
-		
+	
 	if current_scale == prev_scale:
 		return
-			
+	
 	var scale_ratio = current_scale / prev_scale
 	map.scale = Vector2.ONE * current_scale
 	var focal_point_delta = local_mouse_position * (scale_ratio - 1)
 	map.position -= focal_point_delta
 	clamp_position()
-		
+
+
 func clamp_position() -> void:
-		var scaled_map_size = map_size * current_scale
-		
-		var min_x = viewport_size.x - scaled_map_size.x if scaled_map_size.x > viewport_size.x else 0
-		var max_x = 0 if scaled_map_size.x > viewport_size.x else viewport_size.x - scaled_map_size.x
-		var min_y = viewport_size.y - scaled_map_size.y if scaled_map_size.y > viewport_size.y else 0
-		var max_y = 0 if scaled_map_size.y > viewport_size.y else viewport_size.y - scaled_map_size.y
-		
-		if scaled_map_size.x < viewport_size.x:
-			min_x = (viewport_size.x - scaled_map_size.x) / 2
-			max_x = min_x
-		if scaled_map_size.y < viewport_size.y:
-			min_y = (viewport_size.y - scaled_map_size.y) / 2
-			max_y = min_y
-			
-			
-		map.position.x = clamp(map.position.x, min_x, max_x)
-		map.position.y = clamp(map.position.y, min_y, max_y)
-			
-			
-		
-		
-		
+	var scaled_map_size = map_size * current_scale
+	var min_x = -(scaled_map_size.x - viewport_size.x)
+	var max_x = 0
+	var min_y = -(scaled_map_size.y - viewport_size.y)
+	var max_y = 0
+	
+	map.position.x = clamp(map.position.x, min_x, max_x)
+	map.position.y = clamp(map.position.y, min_y, max_y)
