4b73705a8b0bc57bdee67cd88e4f902a0a94761d
ryan-bell <2752881B@student.gla.ac.uk>
2025-01-30 14:16:06 +0000

    Global Timer Unit Tests
    Refs #79
    
    Some unit tests for global timer. Should cover most of global timer but
    is not fully extensive. Tested in Editor and tests should pass, also had
    to change the global timer script to be more correct as I came across
    small issues when making the tests.

--- luscinia/Scripts/global_timer.gd
+++ luscinia/Scripts/global_timer.gd
@@ -1,74 +1,74 @@
 extends Node
 
-signal turn_progressed(time_skipped : int)
+signal turn_progressed(time_skipped: int)
 # Timer variables
 #personal = 1min, discussion = 5min
 @export var cd_minutes: int = 5
 @export var cd_seconds: int = 0
-@export var turns: int = 0
+
+var turns: int = 0
 var countdown_duration
 var current_time_left
-var time_step = 60
+var time_step = 60  #Measured in minutes
 var game_start: bool = false
 
 # Clock variables
-@export var in_game_hours: int = 9  # Measured in horus out of 24
-var in_game_minutes = 0
-var day_counter = 0
-var second_accumulator = 0
-
-func _ready():
-	# Initialize timer
-	countdown_duration = (cd_minutes * 60) + cd_seconds
-	current_time_left = countdown_duration
+var in_game_hours: int = 0  # Measured in hours out of 24
+var in_game_minutes: int = 0
+var in_game_days: int = 0
+var second_accumulator: float = 0
 
 
 func _process(delta):
-	if(game_start):
-		second_accumulator += delta
-		var minutes_left = floor(current_time_left / 60)
-		# Countdown timer logic
-		if current_time_left > 0 and second_accumulator >= 1:
-			second_accumulator -= 1  
-			current_time_left -= 1
-	
-		if current_time_left == 0:
-			skip_time(time_step)
-		
-		
-		#var minutes_left_after = floor(current_time_left / 60)
-		#if minutes_left_after != minutes_left:
-		#	in_game_minutes += 1
-		#	
-		#if in_game_minutes >= 60:
-		#	in_game_minutes -= 60
-		#	in_game_hours += 1
-		#	if in_game_hours >= 24:
-		#		in_game_hours = 0
-		#		day_counter += 1
+	if not game_start:
+		return
+	second_accumulator += delta
 
-func set_time(mins: int, sec: int):
-	cd_minutes = mins
-	cd_seconds = sec
+	if current_time_left > 0 and second_accumulator >= 1.0 - 0.001:
+		second_accumulator -= 1
+		current_time_left -= 1
+	if current_time_left == 0:
+		next_turn(time_step)
+
+
+func set_time(minutes: int, seconds: int):
+	if minutes < 0:
+		minutes = 0
+	if seconds < 0:
+		seconds = 0
+	if seconds >= 60:
+		minutes += floor(seconds / 60)
+		seconds = seconds % 60
+	cd_minutes = minutes
+	cd_seconds = seconds
 	countdown_duration = (cd_minutes * 60) + cd_seconds
 	current_time_left = countdown_duration
 
-func skip_time(skip_minutes: int):
-	var skip_time_seconds = skip_minutes * 60
-	# Adjust the countdown timer
+
+func next_turn(turn_length: int):
+	var skip_time_seconds = turn_length * 60
 	current_time_left = countdown_duration
-	
-	# Adjust the in-game clock
-	in_game_minutes += skip_minutes
+
+	in_game_minutes += turn_length
 	while in_game_minutes >= 60:  # Handle hour overflow
 		in_game_minutes -= 60
 		in_game_hours += 1
 		if in_game_hours >= 24:  # Handle day overflow
 			in_game_hours -= 24
-			day_counter += 1
-	turns+= 1
-	turn_progressed.emit(skip_minutes)
-	print("New in-game time: Day %d, %02d:%02d" % [day_counter, in_game_hours, in_game_minutes])  # Debugging line
-	
+			in_game_days += 1
+
+	turns += 1
+	turn_progressed.emit(turn_length)
+
+
 func start_game():
 	game_start = true
+
+
+func reset_clock():
+	turns = 0
+	game_start = false
+	in_game_hours = 0
+	in_game_minutes = 0
+	in_game_days = 0
+	second_accumulator = 0
