7fe68b4c4672e4e51592565a8dcf5fddd2f41fc4
jackporter04 <2756698P@student.gla.ac.uk>
2025-02-02 15:06:27 +0000

    testing a few resource manager test issues

--- luscinia/Tests/Unit/test_resource_manager.gd
+++ luscinia/Tests/Unit/test_resource_manager.gd
@@ -4,14 +4,14 @@ var resources = {}
 var available_resources = {}
 
 func before_each():
-	resources = ResourceManager.resources.duplicate(true)  
-	available_resources = ResourceManager.available_resources.duplicate(true)  
+	ResourceManager.resources = {"people": 100, "funds": 1000, "vehicles": 80, "supplies": 100000}
+	ResourceManager.available_resources = {"people": 100, "vehicles": 80, "supplies": 100000}	
 
 func test_add_resources():
 	ResourceManager.add_resources("funds", 500)
-	assert_eq(resources["funds"],1500,"funds should equal 1500")
+	assert_eq(ResourceManager.resources["funds"],1500,"funds should equal 1500")
 
 func test_add_available_resources():
 	ResourceManager.add_available_resources("funds", 500)
-	assert_eq(available_resources["funds"],1500,"funds should equal 1500")
+	assert_eq(ResourceManager.available_resources["funds"],1500,"funds should equal 1500")
 	
