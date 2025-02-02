830f6eed652ea1e12317148f54c7d0194a1c5d2d
jackporter04 <2756698P@student.gla.ac.uk>
2025-02-02 15:27:58 +0000

    more test fixes added for resource manager

--- luscinia/Tests/Unit/test_resource_manager.gd
+++ luscinia/Tests/Unit/test_resource_manager.gd
@@ -20,16 +20,12 @@ func test_remove_resources_not_negative():
 	assert_eq(ResourceManager.resources["funds"],0,"funds should equal 0, cannot be negative")
 	
 func test_add_available_resources():
-	ResourceManager.add_available_resources("funds", 500)
+	ResourceManager.add_available_resources("people", 500)
 	assert_eq(ResourceManager.available_resources["funds"],1500,"funds should equal 1500")
 
 func test_remove_available_resources():
-	ResourceManager.remove_available_resources("funds", 500)
+	ResourceManager.remove_available_resources("people", 500)
 	assert_eq(ResourceManager.resources["funds"],500,"funds should equal 500")
-
-func test_remove_available_resources_not_negative():
-	ResourceManager.remove_resources("funds", 1100)
-	assert_eq(ResourceManager.resources["funds"],0,"funds should equal 0, cannot be negative")
 
 func test_get_resource_texture():
 	var texture = ResourceManager.get_resource_texture("people")
