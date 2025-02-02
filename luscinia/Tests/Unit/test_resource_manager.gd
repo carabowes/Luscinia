f832aed50b7b5b57a23e276a72b97e8b928c6eda
jackporter04 <2756698P@student.gla.ac.uk>
2025-02-02 15:25:29 +0000

    more tests added for resource manager

--- luscinia/Tests/Unit/test_resource_manager.gd
+++ luscinia/Tests/Unit/test_resource_manager.gd
@@ -10,8 +10,27 @@ func before_each():
 func test_add_resources():
 	ResourceManager.add_resources("funds", 500)
 	assert_eq(ResourceManager.resources["funds"],1500,"funds should equal 1500")
+	
+func test_remove_resources():
+	ResourceManager.remove_resources("funds", 500)
+	assert_eq(ResourceManager.resources["funds"],500,"funds should equal 500")
 
+func test_remove_resources_not_negative():
+	ResourceManager.remove_resources("funds", 1100)
+	assert_eq(ResourceManager.resources["funds"],0,"funds should equal 0, cannot be negative")
+	
 func test_add_available_resources():
 	ResourceManager.add_available_resources("funds", 500)
 	assert_eq(ResourceManager.available_resources["funds"],1500,"funds should equal 1500")
-	
+
+func test_remove_available_resources():
+	ResourceManager.remove_available_resources("funds", 500)
+	assert_eq(ResourceManager.resources["funds"],500,"funds should equal 500")
+
+func test_remove_available_resources_not_negative():
+	ResourceManager.remove_resources("funds", 1100)
+	assert_eq(ResourceManager.resources["funds"],0,"funds should equal 0, cannot be negative")
+
+func test_get_resource_texture():
+	var texture = ResourceManager.get_resource_texture("people")
+	assert_eq(texture,preload("res://Sprites/UI/User.png"),"image texture should be User.png")
