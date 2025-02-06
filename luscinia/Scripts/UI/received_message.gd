336d9d55911a00d57929e808414d45ddaf55e5ad
ryan-bell <2752881B@student.gla.ac.uk>
2025-02-06 12:18:52 +0000

    Added ReceivedMessage UI Element
    Refs #91
    
    There is now a UI element that shows a message that is sent, the turns
    left to answer on that message and the contact information. Switching
    between states of the message i.e Unread and Read updates the
    information accordingly.
    It might be worth looking into some sort of MessageInstance resource to
    store this data rather than the UI element to keep the data and UI
    seperate.
    The UI element could also be prettified later on, maybe take in a font
    for the answer now colour so it can be bold too.
    Also did not add the arrow that appears on the design as it felt too
    cluttered and was difficult to get the look correct.

---
+++ luscinia/Scripts/UI/received_message.gd
@@ -0,0 +1,65 @@
+class_name ReceivedMessage
+extends Control
+
+enum MessageStatus {
+	UNREAD = 0,
+	READ = 1,
+	REPLIED = 2
+}
+
+@export_enum("SideBadge:0", "ProfileBadge:1") var unread_badge_location : int
+@export var answer_now_color: Color
+@export var time_remaining_color: Color
+
+var current_message_status : MessageStatus 
+var message_info : Message
+var default_message : Message = Message.new("Message failed to load. No message was passed in on initialisation!", [], null, [], [], 3, false)
+
+func _init(message_info : Message = default_message):
+	self.message_info = message_info
+	current_message_status = MessageStatus.UNREAD
+
+
+func _draw():
+	_render_badge_location()
+	_render_message_status()
+	_render_message_info()
+
+
+func _render_message_info():
+	%MessagePreviewLabel.text = message_info.message
+	%ContactNameLabel.text = message_info.sender.name
+	%ContactImage.texture = message_info.sender.image
+	%TimeRemainingLabel.self_modulate = time_remaining_color
+	if current_message_status != MessageStatus.REPLIED:
+		if message_info.turns_to_answer == -1:
+			%TimeRemainingLabel.self_modulate.a = 0
+		elif message_info.turns_to_answer == 1:
+			%TimeRemainingLabel.text = "ANSWER NOW!"
+			%TimeRemainingLabel.self_modulate = answer_now_color
+		else:
+			%TimeRemainingLabel.text = str(message_info.turns_to_answer * GlobalTimer.time_step/60) + " HOURS"
+
+
+func _render_message_status():
+	if current_message_status == MessageStatus.UNREAD:
+		%UnreadBadge.self_modulate.a = 1
+		%ContactUnreadBadge.self_modulate.a = 1
+	elif current_message_status == MessageStatus.READ:
+		%UnreadBadge.self_modulate.a = 0
+		%ContactUnreadBadge.self_modulate.a = 0
+	elif current_message_status == MessageStatus.REPLIED:
+		%UnreadBadge.self_modulate.a = 0
+		%ContactUnreadBadge.self_modulate.a = 0
+		%Layout.modulate = Color.DIM_GRAY
+		%Layout.modulate.a = 0.5
+		%TimeRemainingLabel.text = "REPLIED"
+
+
+func _render_badge_location():
+	if unread_badge_location == 0: #Side Badge
+		%UnreadBadge.visible =  true
+		%ContactUnreadBadge.visible = false
+	else: #Profile badge
+		%UnreadBadge.visible = false
+		%ContactUnreadBadge.visible = true
