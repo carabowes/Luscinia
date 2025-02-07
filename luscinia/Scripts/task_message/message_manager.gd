c93bbf715b6acaeabc015d1b8c84d3191d71e357
”Cara <2757542b@student.gla.ac.uk>
2025-02-07 17:05:02 +0000

    Prerequisite validation
    Refs #89
    
    Added prerequisite validation for checking required tasks, events, turns, and chance.
    Used TaskInstance instead of task IDs to track completion status and validation in
    messagemanager should ensure messages are only sent when the conditions are met.

--- luscinia/Scripts/task_message/message_manager.gd
+++ luscinia/Scripts/task_message/message_manager.gd
@@ -4,9 +4,14 @@ signal message_sent(message: Message)
 
 @export var messages_to_send: Array[Message]
 var messages_to_receive: Array[Message]
-var task_completed: Array[TaskData]
+#var task_completed: Array[TaskData]
+
+var task_instances: Array[TaskInstance]
+var occurred_events: Array[Event.EventType]
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
 func _ready():
+	rng.randomize() ##Randomize the RNG for chance-based validation
 	GlobalTimer.turn_progressed.connect(find_messages_to_send)
 
 
@@ -15,13 +20,13 @@ func find_messages_to_send(time_progressed: int):
 	for message in messages_to_send:
 		var antirequisite_failed : bool = false
 		for antirequisite in message.prerequisites:
-			if validate_prerequisite(antirequisite):
+			if validate_prerequisite(antirequisite, time_progressed):
 				antirequisite_failed = true
 				break
 		if antirequisite_failed:
 			continue
 		for prerequisite in message.prerequisites:
-			if validate_prerequisite(prerequisite):
+			if validate_prerequisite(prerequisite, time_progressed):
 				messages_to_receive.append(message)
 				selected_messages.append(message)
 				message_sent.emit(message)
@@ -32,7 +37,6 @@ func find_messages_to_send(time_progressed: int):
 			message_sent.emit(message)
 	for message in selected_messages:
 		messages_to_send.erase(message)
-
-
-func validate_prerequisite(prerequisite: Prerequisite) -> bool:
-	return true
+	
+func validate_prerequisite(prerequisite: Prerequisite, time_progressed: int) -> bool:
+	return prerequisite.validate(task_instances, occurred_events, time_progressed, rng)
