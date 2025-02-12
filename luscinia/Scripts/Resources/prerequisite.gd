8cde14296fac21ef86e3fc5a1dc1b7ae8c926f4d
ryan-bell <2752881B@student.gla.ac.uk>
2025-02-12 19:12:57 +0000

    Fake Merge with Message System
    Refs #102
    
    Would be too much effort to rebase or merge to message system at the
    moment and I dont exactly need all the features in message system so
    just copied what I needed. A little janky, probably a nicer way to do it
    but for right now alot easier for everyone involved.

--- luscinia/Scripts/Resources/prerequisite.gd
+++ luscinia/Scripts/Resources/prerequisite.gd
@@ -2,14 +2,19 @@ class_name Prerequisite
 extends Resource
 
 ## Array of prerequisite task ids
-@export var task_id: Array[int]
+@export var task_id : Array[String]
 ## Array of prerequisite events
-@export var events: Array[Event.EventType]
+@export var events : Array[Event.EventType]
 ## Chance of prerequisite triggering. 1 is 100% likely.
-@export_range(0, 1) var chance: float
+@export_range(0,1)  var chance : float
+## Minimum turn for the prerequisite to be valid
+@export var min_turn: int = 0
+## Maximim turn for the prerequisite to be valid (i.e. no maximum)
+@export var max_turn: int = -1
 
-
-func _init(task_id: Array[int] = [], chance = 0, events: Array[Event.EventType] = []) -> void:
+func _init(task_id : Array[String] = [], chance: float = 0, events : Array[Event.EventType]= [], min_turn: int = 0, max_turn: int = 0) -> void:
 	self.task_id = task_id
 	self.chance = chance
 	self.events = events
+	self.min_turn = min_turn
+	self.max_turn = max_turn
--- luscinia/Scripts/Resources/response.gd
+++ luscinia/Scripts/Resources/response.gd
@@ -1,12 +1,13 @@
 class_name Response
 extends Resource
 
-@export var response_text: String
-@export var relationship_change: float
-@export var task: TaskData
+@export var response_name : String
+@export var response_text : String
+@export var relationship_change : float
+@export var task : TaskData
 
-
-func _init(response_text = "", relationship_change = 0, task = null) -> void:
+func _init(response_name = "Response", response_text = "", relationship_change = 0, task = null) -> void:
+	self.response_name = response_name
 	self.response_text = response_text
 	self.relationship_change = relationship_change
 	self.task = task
