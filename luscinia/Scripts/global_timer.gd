6ae5a78fd975b72ca5f774543295d7ed1b9ec29f
ryan-bell <2752881B@student.gla.ac.uk>
2025-02-22 15:04:31 +0000

    Added Time String Helper Function
    Closes #113
    
    Added two functions called turns_to_time_string and time_to_time_string
    that takes in a number of turns, or minutes, and a few other parameters
    to output strings of the form 2 hours 30 minutes, 3HRS 45 MINS, 2.5
    HOURS etc.
    
    Also added tests for these functions.
    
    Will hook these up to places that need it.

--- luscinia/Scripts/global_timer.gd
+++ luscinia/Scripts/global_timer.gd
@@ -69,3 +69,65 @@ func skip_time(skip_minutes: int):
 	turns+= 1
 	turn_progressed.emit(skip_minutes)
 	print("New in-game time: Day %d, %02d:%02d" % [day_counter, in_game_hours, in_game_minutes])  # Debugging line
+
+
+## Don't include an s in the minute or hour string, these will be added by the function if applicable
+## i.e. 1 hour, 2 hours
+func turns_to_time_string(
+	turns : int, 
+	hour_string : String = "hour", 
+	minutes_string : String = "mins", 
+	multiple_string: String = "s",
+	use_decimal_minutes : bool = false, 
+	show_minutes : bool  = true
+):
+	return time_to_time_string(
+		turns * time_step, 
+		hour_string, 
+		minutes_string, 
+		multiple_string,
+		use_decimal_minutes, 
+		show_minutes
+	)
+
+
+## Don't include an s in the minute or hour string, these will be added by the function if applicable
+## i.e. 1 hour, 2 hours
+func time_to_time_string(
+	minutes : int, 
+	hour_string : String = "hour", 
+	minutes_string : String = "min",
+	multiple_string: String = "s",
+	use_decimal_minutes : bool  = false, 
+	show_minutes : bool  = true
+):
+	if minutes < 0:
+		minutes = 0
+	var hours : int = int(floor(minutes / 60))
+	var spare_minutes : int = minutes - (hours * 60)
+	var time_string : String = ""
+	
+	if hours != 0 or minutes == 0 or use_decimal_minutes or not show_minutes:
+		time_string += str(hours)
+		
+		if use_decimal_minutes and show_minutes:
+			var decimal_minutes : String = str(floor((float(spare_minutes) / 60.0) * 100))
+			if len(decimal_minutes) == 1:
+				decimal_minutes = "0" + decimal_minutes
+			if decimal_minutes[-1] == "0":
+				decimal_minutes = decimal_minutes[0]
+			if decimal_minutes != "0":
+				time_string += "." + str(decimal_minutes)
+			
+		time_string += " " + hour_string
+		if hours != 1 or (use_decimal_minutes and spare_minutes != 0):
+			time_string += multiple_string
+		if show_minutes and not use_decimal_minutes and spare_minutes != 0:
+			time_string += " "
+		
+	if show_minutes and not use_decimal_minutes and spare_minutes != 0:
+		time_string += str(spare_minutes) + " " + minutes_string
+		if spare_minutes != 1:
+			time_string += multiple_string
+	
+	return time_string
