630b8f745ce941b1f432fa6f19a35ba6571ff20c
jackporter04 <2756698P@student.gla.ac.uk>
2025-02-23 12:34:14 +0000

    fixed test issue (#108)

--- luscinia/Tests/Unit/test_resource_manager.gd
+++ luscinia/Tests/Unit/test_resource_manager.gd
@@ -8,6 +8,7 @@ var sender : Sender
 func before_each():
 	ResourceManager.resources = {"people": 100, "funds": 1000, "vehicles": 80, "supplies": 100000}
 	ResourceManager.available_resources = {"people": 100, "vehicles": 80, "supplies": 100000}
+	ResourceManager.resources["supplies"] = 100000
 	ResourceManager.relationships_to_update.clear()	
 	sender = Sender.new("Test Sender",null,50)
 
