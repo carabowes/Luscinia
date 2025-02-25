0319364e3c0e8c318ee080dbf5bd922fe3c9ba0b
Ryan Bell <2752881B@student.gla.ac.uk>
2025-02-25 12:48:11 +0000

    Message Navbar Button goes to proper page
    Closes #118
    
    The navbar button now goes to the messages page rather than the
    history page.
    
    Also removed all the old message stuff in the main scene

--- luscinia/Scripts/UI/navbar.gd
+++ luscinia/Scripts/UI/navbar.gd
@@ -1,10 +1,7 @@
 extends Control
 
 @export var resource_page : Control
-@export var message_history : Control
-
 func _ready() -> void:
-	
 	%ViewResourcesButton.pressed.connect(_view_resource_button_pressed)
 	%SkipTimeButton.pressed.connect(_skip_time_button_pressed)
 	%ViewMessageHistoryButton.pressed.connect(_message_button_pressed)
@@ -22,7 +19,4 @@ func _skip_time_button_pressed():
 
 
 func _message_button_pressed():
-	if(message_history == null):
-		print("Message Manager not assigned")
-		return
-	message_history.visible = !message_history.visible
+	EventBus.navbar_message_button_pressed.emit()
