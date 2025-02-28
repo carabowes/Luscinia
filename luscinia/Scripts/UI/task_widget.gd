081980ba295248d236aa874ac5fec74fae3f7933
ryan-bell <2752881B@student.gla.ac.uk>
2025-02-28 12:21:34 +0000

    Animations for Task Widgets
    Refs #145
    
    Added some animations for clicking on a widget and for when a widget
    finishes.
    These are done with tweens. The finishing animation might be overkill
    and needs a few more tweaks so is just a prototype for now.
    I think the clicking animation is pretty successful however, and feels
    fun to click.
    The animations are very easy to turn off and are separated from any
    logic.
    Adding the animation has uncovered that the task widget UI isn't the
    easiest to work with and could be improved.

--- luscinia/Scripts/UI/task_widget.gd
+++ luscinia/Scripts/UI/task_widget.gd
@@ -3,16 +3,13 @@ extends Control
 
 signal widget_selected(widget: TaskWidget)
 enum LevelOfDetail { LOW, MEDIUM, HIGH }
+const DEFAULT_SIZE: int = 48
 
 @export var current_level_of_detail: LevelOfDetail
 var task_info: TaskInstance
 
-
-func _ready():
-	add_to_group("task_widgets")
-
-
 func _draw() -> void:
+	print(position)
 	if task_info != null:
 		render_task_info()
 
@@ -23,15 +20,38 @@ func _gui_input(event: InputEvent) -> void:
 		widget_selected.emit($".")
 
 
-func set_level_of_detail(lod):
-	current_level_of_detail = lod
-	match lod:
+func set_level_of_detail(new_lod, use_animation = true):
+	if current_level_of_detail == new_lod:
+		return
+
+	var switching_from_or_to_high_detail = (
+		current_level_of_detail == LevelOfDetail.HIGH
+		or new_lod == LevelOfDetail.HIGH
+	)
+
+	# Animation
+	if switching_from_or_to_high_detail and use_animation:
+		var tween = get_tree().create_tween()
+		tween.tween_property(
+			self.get_child(0), "scale",
+			Vector2.ONE * 0, 0.1
+		).set_trans(Tween.TRANS_QUAD)
+		tween.tween_property(
+			self.get_child(0), "scale",
+			Vector2.ONE, 0.15
+		).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+		# Wait for first tween step to finish before switching LOD
+		await tween.step_finished
+
+	# Switching LOD
+	match new_lod:
 		LevelOfDetail.LOW:
 			_switch_to_low_lod()
 		LevelOfDetail.MEDIUM:
 			_switch_to_med_lod()
 		LevelOfDetail.HIGH:
 			_switch_to_high_lod()
+	current_level_of_detail = new_lod
 
 
 func render_task_info():
@@ -56,6 +76,7 @@ func render_task_info():
 func _switch_to_low_lod():
 	custom_minimum_size = Vector2(48, 48)
 	pivot_offset = Vector2(24, 24)
+	get_child(0).pivot_offset = Vector2(24, 24)
 	%ProgressBarLowMed.visible = true
 	%TaskNameMed.visible = false
 	%TaskInfoHigh.visible = false
@@ -67,6 +88,7 @@ func _switch_to_low_lod():
 func _switch_to_med_lod():
 	custom_minimum_size = Vector2(48, 48)
 	pivot_offset = Vector2(24, 24)
+	get_child(0).pivot_offset = Vector2(24, 24)
 	%ProgressBarLowMed.visible = true
 	%TaskNameMed.visible = true
 	%TaskInfoHigh.visible = false
@@ -77,7 +99,8 @@ func _switch_to_med_lod():
 
 func _switch_to_high_lod():
 	custom_minimum_size = Vector2(220, 120)
-	pivot_offset = Vector2(110, 96)
+	pivot_offset = Vector2(110, 80)
+	get_child(0).pivot_offset =  Vector2(110, 80)
 	%ProgressBarLowMed.visible = false
 	%TaskNameMed.visible = false
 	%TaskInfoHigh.visible = true
--- luscinia/Scripts/UI/task_widget_renderer.gd
+++ luscinia/Scripts/UI/task_widget_renderer.gd
@@ -5,6 +5,8 @@ signal resource_gained(resource: String, amt: int)
 
 @export var widget_size : float
 @export var zoom_level_medium_detail : float
+@export var use_delete_animation : bool = true
+@export var use_resource_bubble_animation : bool = true
 
 var task_widgets : Array[TaskWidget]
 var task_widget_prefab = "res://Scenes/task_widget.tscn"
@@ -29,11 +31,63 @@ func create_widget(task_instance : TaskInstance):
 	render_widgets()
 
 
-func delete_widget(task_instance : TaskInstance, cancelled : bool):
+func render_resource_bubble(resource : String, widget : TaskWidget, rng : RandomNumberGenerator):
+	var resource_bubble : Control = %ResourceBubble.duplicate()
+	add_child(resource_bubble)
+	resource_bubble.get_child(0).texture = ResourceManager.get_resource_texture(resource)
+	# Set initial position to center of widget
+	resource_bubble.position = widget.position  + widget.size/2
+	# Generate random point in a circle with radius 1
+	var offset = Vector2(rng.randf_range(-1, 1), rng.randf_range(-1, 1)).normalized()
+	# Distribute that point to be around the widget
+	var explode_position = resource_bubble.position
+	explode_position += offset * widget.DEFAULT_SIZE * rng.randf_range(0.7, 1)
+	# Set final position to resources bar at top of screen (with some randomness)
+	# Would like a better way to do this so it isn't hardcoded to be the center of  the screen
+	var final_position = Vector2(get_viewport_rect().size.x/2, 0)
+
+	var resource_bubble_tween = get_tree().create_tween()
+	resource_bubble_tween.tween_property(
+		resource_bubble, "position",
+		explode_position, rng.randf_range(0.15, 0.2)
+	).set_trans(Tween.TRANS_SINE)
+	resource_bubble_tween.tween_interval(rng.randf_range(0.2, 0.9))
+	resource_bubble_tween.tween_property(
+		resource_bubble, "position",
+		final_position, rng.randf_range(1.1, 1.5)
+	).set_trans(Tween.TRANS_QUINT)
+	resource_bubble_tween.finished.connect(func(): resource_bubble.queue_free())
+
+
+func render_resource_bubbles(task_instance : TaskInstance, widget : TaskWidget):
+	var rng : RandomNumberGenerator = RandomNumberGenerator.new()
+	for resource in task_instance.task_data.resources_gained:
+		var amount = task_instance.task_data.resources_gained[resource]
+		while amount > 1000:
+			amount /= 1000
+		for i in range(amount):
+			render_resource_bubble(resource, widget, rng)
+
+
+func delete_widget(task_instance : TaskInstance, _cancelled : bool):
 	for widget in task_widgets:
-		if widget.task_info == task_instance:
-			task_widgets.erase(widget)
-			widget.queue_free()
+		if widget.task_info != task_instance:
+			continue
+		# Deleting Animation
+		if use_delete_animation:
+			var widget_tween = get_tree().create_tween()
+			widget_tween.tween_property(
+				widget, "scale",
+				Vector2.ZERO, 0.3
+			).set_trans(Tween.TRANS_SPRING)
+			await widget_tween.finished
+			if use_resource_bubble_animation:
+				widget.reparent(self)
+				render_resource_bubbles(task_instance, widget)
+
+		#Deleting Widget
+		task_widgets.erase(widget)
+		widget.queue_free()
 
 
 func set_level_of_details(affect_high_detail_widgets = false):
