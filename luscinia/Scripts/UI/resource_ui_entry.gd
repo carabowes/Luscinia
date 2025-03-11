9757fa91a207f8c6ab468170f10cada6e321c3a9
ryan-bell <2752881B@student.gla.ac.uk>
2025-03-11 22:37:03 +0000

    Updated Task Details Screen
    Refs #156
    
    Updated the task details screen to be a bit more 'fun', and to be a bit
    more readable. It now shows the sender, shows the resources a bit
    better, and has some animations to give a bit more feedback. The page
    also updates on a timer progress.
    Also updated the resource UI entry to look a bit different, and to have
    more exposed visual parameters, like text color, icon color etc. There
    is also an option to place a circle behind it.
    Fixed some tests with the message page that were broken, and changed it
    so the back buttons go back a page rather than closing the whole thing.

--- luscinia/Scripts/UI/resource_ui_entry.gd
+++ luscinia/Scripts/UI/resource_ui_entry.gd
@@ -1,25 +1,49 @@
 class_name ResourceEntry
 extends GridContainer
 
+const CIRCLE_SIZE : Vector2 = Vector2(50, 50)
+const ICON_SIZE : Vector2 = Vector2(30, 30)
+
 @export_group("Resource info")
 @export var resource_type : String
 @export var amount : int
 @export var increase : int = 0
+
+@export_group("Visual")
 @export var show_arrow : bool = true
+@export var icon_color : Color
+@export var text_color : Color
+@export var circle_color : Color
+@export var use_circle : bool:
+	set(value):
+		use_circle = value
+		if(use_circle):
+			%IconContainer.custom_minimum_size = CIRCLE_SIZE
+			%Circle.visible = true
+			%ResourceIcon.reparent(%Circle,  false)
+		else:
+			%IconContainer.custom_minimum_size = ICON_SIZE
+			%Circle.visible = false
+			%ResourceIcon.reparent(%IconContainer,  false)
+			%ResourceIcon.size = Vector2.ZERO;
+		queue_redraw()
+
 
 func _draw() -> void:
 	%ResourceAmount.text = ResourceManager.format_resource_value(amount, 3)
 	%ResourceIcon.texture = ResourceManager.get_resource_texture(resource_type)
 	if(increase == 0):
 		%IncreaseIcon.visible = false
-		%ResourceAmount.self_modulate = Color.DIM_GRAY
+		%ResourceAmount.self_modulate = text_color
 	elif(increase > 0):
 		%IncreaseIcon.visible = show_arrow;
-		%IncreaseIcon.self_modulate = Color.LIME_GREEN
+		%IncreaseColor.self_modulate = Color.LIME_GREEN
 		%ResourceAmount.self_modulate = Color.LIME_GREEN
 		%IncreaseIcon.rotation = 0;
 	else:
 		%IncreaseIcon.visible = show_arrow;
-		%IncreaseIcon.self_modulate = Color.RED
+		%IncreaseColor.self_modulate = Color.RED
 		%ResourceAmount.self_modulate = Color.RED
 		%IncreaseIcon.flip_v = true;
+	%ResourceIcon.self_modulate = icon_color
+	%Circle.self_modulate = circle_color
