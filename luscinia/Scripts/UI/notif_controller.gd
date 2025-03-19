435d2a798a323ed3bb476900e0f669e326400091
Xiaozu Hu <2888206H@student.gla.ac.uk>
2025-03-19 13:15:59 +0000

    Notifications redesign complete. Ref #157
    - Changed the way notifications are shown from the middle of the screen to the sides of the screen to improve player visibility and experience

--- luscinia/Scripts/UI/notif_controller.gd
+++ luscinia/Scripts/UI/notif_controller.gd
@@ -4,14 +4,8 @@ extends Control
 @export var start_task_notif_label: Label
 @export var end_task_notif_label: Label
 
-var showing: bool = false
-
-var show_time: bool = false
-
-var task_start_notif_text = ""
 var got_name: bool = false
 
-var task_end_notif_text = ""
 var got_report: bool = false
 
 func _ready() -> void:
@@ -26,50 +20,50 @@ func show_task_end_notif(task: TaskInstance, _cancelled : bool):
 	if task != null :
 		if task.task_data.resources_gained == {}:
 			return
-		task_end_notif_text += "Task %s has begun! \n Resources taken:\n" % task.task_data.name
 		for resource in task.task_data.resources_gained:
-			task_end_notif_text += "%s +%d\n" % [resource, task.task_data.resources_gained[resource]]
+			if(resource == "Funds"):
+				var val = int(task.task_data.resources_gained[resource])
+				%EndFunds.text = ": +%s" % ResourceManager.format_resource_value(val,2)
+			if(resource == "people"):
+				%EndPerson.text = ": +%s" % task.task_data.resources_gained[resource]
+			if(resource == "supplies"):
+				%EndSupplies.text = ": +%s" % task.task_data.resources_gained[resource]
+			if(resource == "vehicles"):
+				%EndVehicles.text = ": +%s" % task.task_data.resources_gained[resource]
 	else:
 		return
-	$time_skip_notification.visible = false
-	showing = true
-	end_task_notif_label.text = task_end_notif_text
-	task_end_notif_text = ""
-	got_report = false
 	var tween1 = get_tree().create_tween()
-	tween1.tween_property(%task_end_notification,"position", Vector2(0,205),0.2)
-	await get_tree().create_timer(1.2).timeout #2s
+	tween1.tween_property(%task_end_notification,"position", Vector2(296,-3),0.2)
+	await get_tree().create_timer(1).timeout #2s
 	var tween2 = get_tree().create_tween()
-	tween2.tween_property(%task_end_notification,"position", Vector2(-360,205),0.2)
-	await get_tree().create_timer(0.25).timeout #2s
-	$time_skip_notification.visible = true
-	showing = false
+	tween2.tween_property(%task_end_notification,"position", Vector2(360,-3),0.2)
 
 
 func show_task_start_notif(task: TaskInstance):
 	if task != null :
 		if task.task_data.resources_required == {}:
 			return
-		task_start_notif_text += "Task %s has begun! \n Resources taken:\n" % task.task_data.name
 		for resource in task.task_data.resources_required:
-			task_start_notif_text += "%s -%d\n" % [resource, task.task_data.resources_required[resource]]
+			if(resource == "funds"):
+				var val = int(task.task_data.resources_required[resource])
+				%StartFunds.text = ": -%s" % ResourceManager.format_resource_value(val,2)
+			if(resource == "people"):
+				%StartPerson.text = ": -%s" % task.task_data.resources_required[resource]
+			if(resource == "supplies"):
+				%StartSupplies.text = ": -%s" % task.task_data.resources_required[resource]
+			if(resource == "vehicles"):
+				%StartVehicles.text = ": -%s" % task.task_data.resources_required[resource]
 	else:
 		return
-	start_task_notif_label.text = task_start_notif_text
-	task_start_notif_text = ""
 	var tween1 = get_tree().create_tween()
-	#og -360, 205
-	#center 0, 205
-	tween1.tween_property(%task_start_notification,"position", Vector2(0,205),0.2)
-	await get_tree().create_timer(2).timeout #2s
+	tween1.tween_property(%task_start_notification,"position", Vector2(0,-3),0.2)
+	await get_tree().create_timer(1).timeout #2s
 	var tween2 = get_tree().create_tween()
-	tween2.tween_property(%task_start_notification,"position", Vector2(-360,205),0.2)
+	tween2.tween_property(%task_start_notification,"position", Vector2(-72,-3),0.2)
 
 
 func show_turn_notif(_new_turn : int):
-	if(!show_time):
-		show_time = true
-		await turn_notif_coroutine()
+	await turn_notif_coroutine()
 
 
 func turn_notif_coroutine():
@@ -85,4 +79,3 @@ func turn_notif_coroutine():
 	tween2.tween_property($time_skip_notification,"position", Vector2(-136,3),0.2)
 	await tween2.finished
 	print("can show")
-	show_time = false
